4281fc32e7d35093175258576f330159ea7cddff
takahirom <takam.dev@gmail.com>
2015-10-01 00:56:13 +0900

    Delete unnecessary field

--- src/main/java/com/kogitune/intellij/codeinsight/postfix/templates/surround/FindViewByIdFieldTemplate.java
+++ src/main/java/com/kogitune/intellij/codeinsight/postfix/templates/surround/FindViewByIdFieldTemplate.java
@@ -8,11 +8,6 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.impl.ActionManagerImpl;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ui.playback.commands.ActionCommand;
-import com.intellij.openapi.util.Condition;
-import com.intellij.psi.PsiElement;
-import com.kogitune.intellij.codeinsight.postfix.utils.AndroidPostfixTemplatesUtils;
-
-import static com.intellij.codeInsight.template.postfix.util.JavaPostfixTemplatesUtils.IS_NUMBER;
 
 /**
  * Postfix template for android Log.
@@ -20,14 +15,6 @@ import static com.intellij.codeInsight.template.postfix.util.JavaPostfixTemplate
  * @author takahirom
  */
 public class FindViewByIdFieldTemplate extends FindViewByIdTemplate {
-
-    public static final Condition<PsiElement> IS_NON_NULL = new Condition<PsiElement>() {
-        @Override
-        public boolean value(PsiElement element) {
-            return IS_NUMBER.value(element) && !AndroidPostfixTemplatesUtils.isAnnotatedNullable(element);
-        }
-
-    };
 
     public FindViewByIdFieldTemplate() {
         super("findf");
